50b005453a9bcbb4f125303a10af0af7e8bf1567
Justin Worsdale <whayler1@gmail.com>
2014-03-16 13:43:01 -0400

    cleanup

--- gruntfile.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(grunt) {
-	grunt.initConfig({
-		pkg: grunt.file.readJSON('package.json')
-	});
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-jshint');
-};
