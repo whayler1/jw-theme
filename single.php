8b596370171f22e2cb98f38c52e5b84bfef216ff
Justin Worsdale <whayler1@gmail.com>
2014-05-07 20:49:20 -0400

    mobile blogroll single post

--- single.php
+++ single.php
@@ -26,7 +26,7 @@
 							</section>
 
 							<footer class="article-footer">
-								<p><?php printf( __( '<time class="updated" datetime="%1$s" pubdate>%2$s</time>'), get_the_time('Y-m-j'), get_the_time(get_option('date_format'))); ?>. Tagged: <?php the_tags( '<span class="tags-title">' . __( '', 'bonestheme' ) . '</span> ', ', ', '' ); ?></p>
+								<div><?php printf( __( '<time class="updated" datetime="%1$s" pubdate>%2$s</time>'), get_the_time('Y-m-j'), get_the_time(get_option('date_format'))); ?>. Tagged: <?php the_tags( '<span class="tags-title">' . __( '', 'bonestheme' ) . '</span> ', ', ', '' ); ?></div>
 
 							</footer>
 							
